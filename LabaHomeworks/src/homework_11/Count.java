da37f53b91143ab0fcfe1a644b1c8bc69df96f8e
dsmolinapl <daryasmolinapl@gmail.com>
2024-09-25 14:01:52 +0200

    Updating to loggers

--- LabaHomeworks/src/homework_11/Count.java
+++ LabaHomeworks/src/homework_11/Count.java
@@ -1,13 +1,17 @@
 package homework_11;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Count {
+	private static  final  Logger LOGGER = LogManager.getLogger();
 	static private String countDownToNewYear[] = { "10 seconds", "9 seconds", "8 seconds", "7 seconds", "6 seconds",
 			"5 seconds", "4 seconds", "3 seconds", "2 seconds", "1 second", "New Year!!!" };
 
 	public static void printCountDown() throws InterruptedException {
 		for (int i = 0; i < countDownToNewYear.length; i++) {
 			Thread.sleep(1000);// Pause for 1 seconds
-			System.out.println(countDownToNewYear[i]);
+			LOGGER.info(countDownToNewYear[i]);
 		}
 	}
 
--- LabaHomeworks/src/homework_11/OwnThread.java
+++ LabaHomeworks/src/homework_11/OwnThread.java
@@ -1,10 +1,14 @@
 package homework_11;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class OwnThread extends Thread {
+	private static  final  Logger LOGGER = LogManager.getLogger();
 	@Override
 	public void run() {
 		for (int i = 0; i < 100; i++) {
-			System.out.println("My own Thread: " + i);
+			LOGGER.info("My own Thread: " + i);
 		}
 	}
 
